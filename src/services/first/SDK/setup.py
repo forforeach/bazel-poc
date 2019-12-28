13e9c04f22e64132b66778579fb1a79021b65b02
forforeach <forforeach@gmail.com>
2019-12-28 18:05:28 +0200

    build(*): make setup file be callable from files

--- src/services/first/SDK/setup.py
+++ src/services/first/SDK/setup.py
@@ -1,21 +1,26 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
-setuptools.setup(
-    name="first_service_sdk",  # Replace with your own username
-    version="0.0.3",
-    author="forforeach",
-    description="The SDK for the first service",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/forforeach/bazel-poc",
-    packages=setuptools.find_namespace_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-)
+def build():
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+    setuptools.setup(
+        name="first_service_sdk",  # Replace with your own username
+        version="0.0.3",
+        author="forforeach",
+        description="The SDK for the first service",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/forforeach/bazel-poc",
+        packages=setuptools.find_namespace_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        python_requires='>=3.6',
+    )
+
+
+if __name__ == "__main__":
+    build()
